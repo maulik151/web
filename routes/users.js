fc89259a41864ac75ee3bc0c267fc021b3b8dc6f
parita15 <koladiyamaulik@gmail.com>
2022-10-16 10:48:51 -0400

    setup mongodb

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Placeholder');
+// connect to our Book Model
+let userList = require('../models/users');
+
+/* GET Route for the Service List page - READ OPeration */
+router.post('/login', (req, res, next) => {
+  // userList.findOne({
+  //   username: req.body.username
+  // }).exec((err, user) => {
+  //   if (err) {
+  //     res.status(500).send({ message: err });
+  //     return;
+  //   }
+
+  //   if (user) {
+  //     res.status(400).send({ message: "Failed! Username is already in use!" });
+  //     return;
+  //   }
+  // });
+  let username = req.body.username;
+  let password = req.body.password;
+  res.send(`Username: ${username} Password: ${password}`);
 });
 
+
 module.exports = router;
+
