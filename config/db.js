a5b299d5a4506c98e594b0bd82f7b17d2b9a0101
parita15 <koladiyamaulik@gmail.com>
2022-10-30 16:12:26 -0400

    uri changes

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ module.exports =
 {
     // "URI": "mongodb://127.0.0.1:27017/assignment2",
     // "URI": "mongodb+srv://maulik:<password>@cluster0.90narqf.mongodb.net/?retryWrites=true&w=majority",
-    "URI": "mongodb+srv://maulik:M@ulik151@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
